576d863c5056884042bdabc7825bf483ea9133ff
Sophia Tran <sophiat8832@gmail.com>
2023-06-16 14:48:07 -0700

    implemented updateCompletion & deleteTask to add props to TaskList
    Co-authored-by: louloera <louloera@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -18,13 +19,39 @@ const TASKS = [
 const App = () => {
   const [allTasks, setAllTasks] = useState(TASKS);
 
+  const updateCompletion = (taskId) => {
+    const updatedTasks = allTasks.map(task => {
+      if (taskId === task.id){
+        task.isComplete = !task.isComplete; 
+      }
+      return {...task};
+    });
+    setAllTasks(updatedTasks);
+  };
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = allTasks.map(task => {
+      if (taskId !== task.id){
+        return {...task};
+      }
+      
+    });
+
+    const filteredUpdatedData = updatedTasks.filter(function (element) {
+      return element !== undefined;
+    });
+
+    setAllTasks(filteredUpdatedData);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={ allTasks } />}</div>
+        <div>{<TaskList tasks={ allTasks } updateCompletion={updateCompletion} 
+        deleteTask={deleteTask}/>}</div>
       </main>
     </div>
   );
